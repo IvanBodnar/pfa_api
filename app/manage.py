47ebe850831abc4ff245fb8b1def50fcf7e6b219
Ivan Bodnar <ivangbodnar@gmail.com>
2016-08-02 21:40:12 -0300

    mens

--- app/manage.py
+++ app/manage.py
@@ -4,7 +4,7 @@ from flask_migrate import MigrateCommand
 sys.path.append(os.path.abspath(os.path.join(os.path.dirname(__file__), os.path.pardir)))
 
 from flask_script import Manager, Server
-from app import app
+import app
 
 manager = Manager(app)
 
