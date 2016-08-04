6d6bbd3a25b9e4295f6627839f503476fdbff708
Ivan Bodnar <ivangbodnar@gmail.com>
2016-08-04 18:23:53 -0300

    sddd

--- app/fabfile.py
+++ app/fabfile.py
@@ -5,7 +5,7 @@ env.user = 'ivan'
 env.password = 'pared111'
 
 def mk():
-    local('git add --all && git commit')
+    local('git add --all :/ && git commit')
     local('git push')
     run('sudo service nginx stop')
     run('cd /apps/pfa_api && source/venv/bin/activate')
