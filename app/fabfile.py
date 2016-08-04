6c4d27f2d7f281f9492328a36b5231e73052f851
Ivan Bodnar <ivangbodnar@gmail.com>
2016-08-04 18:18:34 -0300

    prueba fabric

---
+++ app/fabfile.py
@@ -0,0 +1,13 @@
+from fabric.api import *
+
+env.hosts = ['ivan@104.236.39.222']
+env.user = 'ivan'
+env.password = 'pared111'
+
+def mk():
+    local('git add --all && git commit')
+    local('git push')
+    run('sudo service nginx stop')
+    run('cd /apps/pfa_api && source/venv/bin/activate')
+    run('git pull origin master')
+    run('sudo service nginx start')
