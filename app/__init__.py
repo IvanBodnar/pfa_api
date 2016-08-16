e6bb87459cb8508ef8003b4edfd98e8048d1d9b4
Ivan Bodnar <ivangbodnar@gmail.com>
2016-08-16 11:12:45 -0300

    Agregado CORS

--- app/__init__.py
+++ app/__init__.py
@@ -1,10 +1,12 @@
 from flask import Flask
 from flask_sqlalchemy import SQLAlchemy
 from flask_migrate import Migrate
+from flask_cors import CORS
 
 app = Flask(__name__)
 app.config.from_pyfile('settings.py')
 db = SQLAlchemy(app)
 migrate = Migrate(app, db)
+CORS(app)
 
 from api import views
