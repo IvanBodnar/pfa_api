73cc74bee2d9d9b08acfb69fe0ad1cbf6cf2f2ee
Ivan Bodnar <ivangbodnar@gmail.com>
2016-08-17 20:12:23 -0300

    Agregado sslify

--- app/__init__.py
+++ app/__init__.py
@@ -2,11 +2,13 @@ from flask import Flask
 from flask_sqlalchemy import SQLAlchemy
 from flask_migrate import Migrate
 from flask_cors import CORS
+from flask_sslify import SSLify
 
 app = Flask(__name__)
 app.config.from_pyfile('settings.py')
 db = SQLAlchemy(app)
 migrate = Migrate(app, db)
 CORS(app)
+sslify = SSLify(app)
 
 from api import views
